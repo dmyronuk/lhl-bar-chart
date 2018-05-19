bf493bda9299f5d092a92dc86e745f4b8a5ed7d6
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-05-19 17:07:54 +0000

    Add functions createTitle and createChartInner

--- bar-chart.js
+++ bar-chart.js
@@ -1,3 +1,29 @@
+let createTitle = (element, title, fontSize, fontColor) => {
+  let titleDiv = $(`<div>${title}</div>`);
+  titleDiv.css("fontSize", fontSize+"px");
+  titleDiv.css("color", fontColor);
+  titleDiv.addClass("chart-title");
+  $("#"+element).append(titleDiv);
+}
+
+let createChartInner = (element) => {
+  let container = $("#" + element);
+  let titleDiv = container.children(".chart-title")[0];
+
+  //calculate height remaining in parent container after title is rendered
+  let height = container[0].clientHeight - titleDiv.clientHeight;
+  let width = container[0].clientWidth;
+
+  let chartInner = $("<div></div");
+  chartInner.addClass("chart-inner");
+  chartInner.css("height", height);
+  chartInner.css("width", width);
+  container.append(chartInner);
+}
+
 let drawBarChart = (data, options, element) => {
 
 }
+
+createTitle("chart-container-a", "Some Graph", 35, "red");
+createChartInner("chart-container-a");
