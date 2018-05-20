88ef808582b4b6abb29162651548b93eefcf782f
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-05-20 13:18:07 +0000

    Add function createBars

--- bar-chart.js
+++ bar-chart.js
@@ -38,15 +38,42 @@ let createChartInner = (element) => {
   chartInner.append(axes)
 }
 
+//barWidth is fn of data.length, barHeight is fn of max(data)
+let createBars = (element, data, barSpacing, barColor) => {
+  let axes = $("#" + element).find(".axes");
+
+  //We don't want the tallest bar going all the way to the top of the axes so scale them down to 80% of container
+  let height = axes.innerHeight() * 0.8;
+  let width = axes.innerWidth();
+  let maxVal = data.reduce((a,b) => a > b ? a:b);
+
+  let totalSpacerWidth = barSpacing * (data.length + 1);
+  let barWidth = (width - totalSpacerWidth) / data.length;
+
+  for(let i=0; i < data.length; i++){
+    let curBar = $("<div></div>");
+    let curBarHeight = height * data[i] / maxVal;
+
+    curBar.css("margin-left", barSpacing);
+    curBar.css("display", "inline-block");
+    curBar.css("width", barWidth + "px");
+    curBar.css("height", curBarHeight);
+    curBar.css("background", barColor);
+    curBar.addClass("bar");
+    axes.append(curBar);
+  }
+}
 
 let drawBarChart = (data, options, element) => {
 
 }
 
-createContainer("main-container", 700, 400)
+createContainer("main-container", 600, 400)
 createTitle("chart-1-container", "Some Graph", 35, "red");
 createChartInner("chart-1-container");
+createBars("chart-1-container", [5,8,2,9,10, 4, 6, 12], 10, "red");
 
 createContainer("main-container", 400, 600)
 createTitle("chart-2-container", "Some Graph", 35, "red");
 createChartInner("chart-2-container");
+createBars("chart-2-container", [45,21,10,5,34], 20, "blue");
