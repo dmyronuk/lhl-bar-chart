49f27db674faddb2dc935effaaef9d5b512b96b8
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-05-21 12:49:12 +0000

    Add functions to create y-axis ticks Add function to validate chart data type

--- bar-chart.js
+++ bar-chart.js
@@ -4,7 +4,6 @@ let createContainer = (element, width, height) => {
   container.css("height", height);
   container.addClass("chart-container");
 
-  //Dynamically assign id
   let chartNum = $("#"+element).children(".chart-container").length;
   let containerId = `${element}-chart-${chartNum}-container`;
   container.attr("id", containerId);
@@ -39,21 +38,51 @@ let createChartInner = (element) => {
   chartInner.append(axes)
 }
 
-let createTicks = (axes, maxVal) => {
-  let height = axes.innerHeight();
-  let tickHeight = height/10;
-  for(let i = 1; i < 10; i++){
-    let curHeight = i * tickHeight;
+let getTickInterval = (maxVal) => {
+  let exp = Math.floor(Math.log10(maxVal));
+  let tickInterval = 10 ** exp; // largest power of 10 that's less than our max Val
 
-    let tickLabel = $("<div/>")
+  let multiple = maxVal / tickInterval; // this is how many ticks we get at the current Interval
+  if(multiple <= 2){tickInterval /= 5}
+  else if(multiple <= 5){tickInterval /= 2};
+
+  return tickInterval;
+}
+
+let createTicks = (containerId, maxVal, tickInterval) => {
+  let axes = $("#" + containerId).find(".axes");
+  let axisHeight = axes.innerHeight();
+  let maxHeight = axes.innerHeight() * 0.8; //Bars are scaled to 80% axis height so ticks should be scaled by same ratio
+  let curTickVal = tickInterval;
+  let tickHeight = maxHeight * tickInterval / maxVal;
+  let curHeight = tickHeight;
+
+  while(curHeight < axisHeight){
+    let tickLabel = $("<div/>").addClass("tick-label");
     tickLabel.css("bottom", curHeight);
-    tickLabel.text = "label";
-    axes.append(tickLabel)
+    tickLabel.text(`${curTickVal}`);
+    axes.append(tickLabel);
 
     let tick = $("<div/>").addClass("tick");
     tick.css("bottom", curHeight);
     axes.append(tick);
+
+    curTickVal += tickInterval;
+    curHeight += tickHeight;
   }
+}
+
+/* data arg can be either an array of object (labelled values), an array of subarrays (stacked graph) or an array of ints
+Fn checks that all values in the data array are of the same class and returns a string representing the class ie "Array"
+obj.constructor.name is only available in ES6 */
+let getDataClass = (data) => {
+  firstElemClass = data[0].constructor.name;
+  for(let i=1; i<data.length; i++){
+    if(data[i].constructor.name !== firstElemClass){
+      throw "Exception: data values must be instances of the same class"
+    }
+  }
+  return firstElemClass;
 }
 
 let getMaxVal = (data, graphType) => {
@@ -72,7 +101,7 @@ let getMaxVal = (data, graphType) => {
 }
 
 //barWidth is fn of data.length, barHeight is fn of max(data)
-let renderData = (graphType, element, data, barSpacing, barColor) => {
+let renderData = (graphType, element, data, maxVal, barSpacing, barColor) => {
   let axes = $("#" + element).find(".axes");
 
   //We don't want the tallest bar going all the way to the top of the y axis so scale y-vals down to 80% of container
@@ -80,21 +109,17 @@ let renderData = (graphType, element, data, barSpacing, barColor) => {
   let width = axes.innerWidth();
   let totalSpacerWidth = barSpacing * (data.length + 1);
   let barWidth = (width - totalSpacerWidth) / data.length;
-  let maxVal = getMaxVal(data, graphType);
-  createTicks(axes, maxVal);
 
   for(let i=0; i < data.length; i++){
-    //curData will be an array if graph has stacked bars, else curData will be a single value (number) for regular bars
+    //curData will be an array if graph has stacked bars, else curData will be a single value for regular bars
     let curData = data[i];
     let curBar;
 
     if(graphType === "bar"){
-      maxVal = data.reduce((a,b) => a > b ? a:b);
       let yVal = curData
       curBar = createBar(height, width, yVal, maxVal, barWidth, barSpacing, barColor);
 
     }else if(graphType === "stacked"){
-
       //container for stacked values
       curBar = $("<div/>").addClass("stacked-bar");
 
@@ -105,8 +130,7 @@ let renderData = (graphType, element, data, barSpacing, barColor) => {
         curBar.append(curStackedBar);
       }
     }
-
-    //since createBar fn doesn't know whether bars are stacked or not, we need to set this css outside the fn body
+    //since createBar fn doesn't know whether bars are stacked or not, we need to set this css value outside the fn body
     curBar.css("display", "inline-block");
     axes.append(curBar);
   }
@@ -116,7 +140,6 @@ let renderData = (graphType, element, data, barSpacing, barColor) => {
 let createBar = (height, width, value, maxVal, barWidth, barSpacing, barColor) => {
   let bar = $("<div/>").addClass("bar");
   let barHeight = height * value / maxVal;
-
   bar.css("margin-left", barSpacing);
   bar.css("width", barWidth);
   bar.css("height", barHeight);
@@ -125,10 +148,21 @@ let createBar = (height, width, value, maxVal, barWidth, barSpacing, barColor) =
 }
 
 let drawBarChart = (data, options, element) => {
-  let containerId = createContainer(element, options["width"], options["height"])
-  createTitle(containerId, options["title"], options["titleFontSize"], options["titleColor"]);
+  try{
+    let dataClass = getDataClass(data);
+  }catch(err){
+    console.log(err);
+    return;
+  }
+
+  let containerId = createContainer(element, options.width, options.height);
+  createTitle(containerId, options.title, options.titleFontSize, options.titleColor);
   createChartInner(containerId);
-  renderData(options["graphType"], containerId, data, options["barSpacing"], options["barColor"]);
+
+  let maxVal = getMaxVal(data, options.graphType);
+  let tickInterval = getTickInterval(maxVal);
+  createTicks(containerId, maxVal, tickInterval);
+  renderData(options.graphType, containerId, data, maxVal, options.barSpacing, options.barColor);
 }
 
 let optionsA = {
@@ -140,8 +174,9 @@ let optionsA = {
   titleColor:"red",
   barSpacing:10,
   barColor:"red",
+  barValuePosition:"top",
 };
-let dataA = [5,8,2,9,10, 4, 6, 12];
+let dataA = [1200,580,1452,1300,300];
 drawBarChart(dataA, optionsA, "main-container" );
 
 let optionsB = {
@@ -153,6 +188,7 @@ let optionsB = {
   titleColor:"darkblue",
   barSpacing:15,
   barColor:"blue",
+  barValuePosition:null,
 };
 let dataB = [[2,6,4], [3,1,2], [5,3,2], [4,4,7]];
 drawBarChart(dataB, optionsB, "main-container" );
